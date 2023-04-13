20705886626a5a6bd4ea12fe906911323d6d6861
Davs06 <santosdv40@gmail.com>
2023-04-13 15:09:41 -0300

    usando metodos

--- src/components/INfoPessoa.vue
+++ src/components/INfoPessoa.vue
@@ -10,33 +10,55 @@
       <li>Java</li>
       <li>PHP</li>
     </ul>
-    
-    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
-    <p>Para acessar meu portifolio, <a v-bind:href="meu_portifolio" target="_blank">basta clicar aqui </a></p> <!-- Usar v-bind para links de outras pag-->
-    <p>Para acessar meu github, <a v-bind:href="meu_github" target="_blank">basta clicar aqui </a></p> <!-- Usar v-bind para links de outras pag-->
-    
-    <Picture/>
 
+    <div>
+      <button @click="showEmail">{{ txt_btn }}</button>
+    </div>
+
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p>
+      Para acessar meu portifolio,
+      <a v-bind:href="meu_portifolio" target="_blank">basta clicar aqui </a>
+    </p>
+    <!-- Usar v-bind para links de outras pag-->
+    <p>
+      Para acessar meu github,
+      <a v-bind:href="meu_github" target="_blank">basta clicar aqui </a>
+    </p>
+    <!-- Usar v-bind para links de outras pag-->
+
+    <Picture />
   </div>
 </template>
 
 <script>
-import Picture from './Picture.vue';
-
+import Picture from "./Picture.vue";
 
 export default {
   name: "InfoPessoa",
   components: {
-    Picture
+    Picture,
   },
   data() {
     return {
       in_job: false,
       email: "davs@emai.com",
-      mostrar_email: true,
-      meu_portifolio: 'https://portifolio-davs.netlify.app',
-      meu_github: 'https://github.com/Davs06'
+      mostrar_email: false,
+      meu_portifolio: "https://portifolio-davs.netlify.app",
+      meu_github: "https://github.com/Davs06",
+      txt_btn: "Mostrar email",
     };
+  },
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email;
+
+      if (!this.mostrar_email) {
+        this.txt_btn = "Mostrar email";
+      } else {
+        this.txt_btn = "Esconder email";
+      }
+    },
   },
 };
 </script>

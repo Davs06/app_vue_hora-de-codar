102756806e430ea08df4bdfd6e84452e8606e9c9
Davs06 <santosdv40@gmail.com>
2023-04-13 11:15:57 -0300

    instalando vue cli e criando primeiro component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Hello world !!</h1>
+  <primeiroComponente/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import primeiroComponente from './components/primeiroComponente.vue';
 
 export default {
-  name: 'App',
+  name:'App',
   components: {
-    HelloWorld
+    primeiroComponente
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/primeiroComponente.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Hello vue!!</h2>
+</template>
+
+<script>
+export default {
+  name: "primeiroComponente"
+};
+</script>

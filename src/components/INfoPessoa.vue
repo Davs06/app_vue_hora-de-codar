29546a2d3ccccb7b428680cb4f46997b3863e5c5
Davs06 <santosdv40@gmail.com>
2023-04-13 14:54:57 -0300

    manipulando links e imagens : v-bind

--- src/components/INfoPessoa.vue
+++ src/components/INfoPessoa.vue
@@ -10,19 +10,32 @@
       <li>Java</li>
       <li>PHP</li>
     </ul>
-
+    
     <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p>Para acessar meu portifolio, <a v-bind:href="meu_portifolio" target="_blank">basta clicar aqui </a></p> <!-- Usar v-bind para links de outras pag-->
+    <p>Para acessar meu github, <a v-bind:href="meu_github" target="_blank">basta clicar aqui </a></p> <!-- Usar v-bind para links de outras pag-->
+    
+    <Picture/>
+
   </div>
 </template>
 
 <script>
+import Picture from './Picture.vue';
+
+
 export default {
   name: "InfoPessoa",
+  components: {
+    Picture
+  },
   data() {
     return {
       in_job: false,
       email: "davs@emai.com",
       mostrar_email: true,
+      meu_portifolio: 'https://portifolio-davs.netlify.app',
+      meu_github: 'https://github.com/Davs06'
     };
   },
 };
---
+++ src/components/Picture.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <img :src="avatar" :alt="alt"  />
+      <!-- usando v-bind de forma resumida = : scr/herf ....-->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Picture',
+    data() {
+      return {
+        avatar: "/img/avatar.jpg",
+        alt: "profile_photo",
+      };
+    },
+  };
+  </script>

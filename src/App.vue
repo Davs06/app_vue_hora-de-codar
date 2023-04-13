cefbfbac9c508e7abeb13d6763d969f46b12e5a6
Davs06 <santosdv40@gmail.com>
2023-04-13 12:20:11 -0300

    Dados dinamicos lifeCycles / usando eventos tmb

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div>
     <primeiroComponente />
     <p> This is {{ test }}</p>
+
+    <LifeCycle/>
   </div>
 </template>
 
 <script>
 import primeiroComponente from "./components/primeiroComponente.vue";
+import LifeCycle from './components/LifeCycle.vue';
 
 export default {
   name: "App",
   components: {
     primeiroComponente,
+    LifeCycle
   },
   data() {
     return {
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Meu nome: {{ nome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      nome: "N sei ainda u.u",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Davs";
+    },1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "Taw";
+    },2000);
+  },
+};
+</script>

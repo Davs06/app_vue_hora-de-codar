6f5a846c4e9a3f0f3b3eb50327b8e1c270540c97
Davs06 <santosdv40@gmail.com>
2023-04-13 22:37:53 -0300

    multiplos eventos em uma tag

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button @click="primeiroEvento(), segundo(), terceiro()">Ativar multiplos enventos</button>
+
+        <p>{{multiplos_Eventos}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MultiplosEventos',
+    components:{
+
+    },
+    data() {
+        return {
+           multiplos_Eventos: '', 
+        }
+    },
+    methods: {
+        primeiroEvento(e){
+            this.multiplos_Eventos = 'primeiro evento ativado'
+        },
+        segundo(e){
+            setTimeout(()=>this.multiplos_Eventos = 'segundo evento',1000)
+        } ,
+        terceiro(e){
+            setTimeout(()=>this.multiplos_Eventos = 'terceiro evento',2000)
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+div{
+    margin: 100px;
+}
+
+</style>
--- src/components/primeiroComponente.vue
+++ src/components/primeiroComponente.vue
@@ -3,12 +3,19 @@
     <h1>Hello guys!!</h1>
 
     <p>My name is {{ nome }}, and my job is {{ job }}</p>
+
+    <MultiplosEventos/>
   </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
+
 export default {
   name: "primeiroComponente",
+  components: {
+    MultiplosEventos
+  },
   data() {
     return {
       nome: "Davs",
